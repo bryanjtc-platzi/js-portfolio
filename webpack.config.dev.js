b1f496db5b5410f70a33dcffd74502dbb3452371
Bryan Thomas <bryan.thomas25000@icloud.com>
2021-03-11 22:51:26 -0500

    avance

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -17,6 +17,7 @@ module.exports = {
     assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   mode: "development",
+  watch: true,
   resolve: {
     extensions: [".js"], // LOS ARCHIVOS QUE WEBPACK VA A LEER
     alias: {
